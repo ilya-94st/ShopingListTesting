ec8069740c53a1e1d46f2ec7746afe04b11a9687
Ilya Stasialovich <i.stasialovich.vironit@vironit.ru>
2021-11-19 12:08:36 +0300

    create tests for viewModels

--- app/src/main/java/com/example/shopinglisttesting/ui/fragments/ShoppingViewModel.kt
+++ app/src/main/java/com/example/shopinglisttesting/ui/fragments/ShoppingViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ResponseImage
 import com.example.shopinglisttesting.data.local.ShoppingItems
+import com.example.shopinglisttesting.other.Constants
 import com.example.shopinglisttesting.other.Event
 import com.example.shopinglisttesting.other.Resources
 import com.example.shopinglisttesting.repository.ShoppingRepository
@@ -51,10 +52,42 @@ class ShoppingViewModel @Inject constructor(val shoppingRepository: ShoppingRepo
     fun totalPrise() = shoppingRepository.readTotalPrise()
 
     fun insertShoppingItems(name: String, amountString: String, price: String) {
-
+        // проверка на пустое поле
+       if(name.isEmpty() || amountString.isEmpty() || price.isEmpty()) {
+           _insertShoppingStatus.postValue(Event(Resources.error("the field must not be empty", null)))
+       return
+       }
+        // проверка на максимальную длинну имени
+       if(name.length > Constants.MAX_LENGTH_NAME) {
+           _insertShoppingStatus.postValue(Event(Resources.error("the field contains more" + "${Constants.MAX_LENGTH_NAME} symbols", null)))
+           return
+       }
+        // проверка на максимальную длину цены
+        if(price.length > Constants.MAX_LENGTH_PRICE) {
+            _insertShoppingStatus.postValue(Event(Resources.error("the field contains more" + "${Constants.MAX_LENGTH_PRICE} symbols", null)))
+            return
+        }
+        // проверка на корректно введенное число
+        val amount = try {
+         amountString.toInt()
+        } catch (e: Exception){
+            _insertShoppingStatus.postValue(Event(Resources.error("please enter the required number" + "amount", null)))
+         return
+        }
+        val shoppingItems = ShoppingItems(name, amount, price.toFloat(), _imagesUrl.value?: "")
+        insertShoppingItemsDB(shoppingItems)
+        setImagesUrl("")
+        _insertShoppingStatus.postValue(Event(Resources.successes(shoppingItems)))
     }
 
     fun searchImagesPix(imageQuery: String) {
-
+      if (imageQuery.isEmpty()){
+          return
+      }
+        _imagesPix.value = Event(Resources.landing(null))
+        viewModelScope.launch {
+            val response = shoppingRepository.searchPixImage(imageQuery)
+            _imagesPix.value = Event(response)
+        }
     }
 }
---
+++ app/src/test/java/com/example/shopinglisttesting/ui/fragments/ShoppingViewModelTest.kt
@@ -0,0 +1,93 @@
+package com.example.shopinglisttesting.ui.fragments
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.shopinglisttesting.MainCoroutineRule
+import com.example.shopinglisttesting.getOrAwaitValueTest
+import com.example.shopinglisttesting.other.Constants
+import com.example.shopinglisttesting.other.Resources
+import com.example.shopinglisttesting.repositories.FakeShoppingRepository
+import org.junit.Before
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+
+@ExperimentalCoroutinesApi
+class ShoppingViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+    @get: Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: ShoppingViewModel
+
+    @Before
+    fun setup() {
+        viewModel = ShoppingViewModel(FakeShoppingRepository())
+    }
+
+    @Test
+    // если поле количество пусото
+    fun `insert shopping item with empty field return error`() {
+        viewModel.insertShoppingItems("name", "", "7.0")
+
+        val value = viewModel.insertShoppingStatus().getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Resources.Status.ERROR)
+    }
+
+    @Test
+    // если поле имя слишком велико
+    fun `insert shopping item with too long name return error`() {
+        // тестируем максимальную длину строки
+        val string = buildString {
+            for(i in 1..Constants.MAX_LENGTH_NAME + 1) {
+                append(i)
+            }
+        }
+        viewModel.insertShoppingItems(string, "12", "7.0")
+
+        val value = viewModel.insertShoppingStatus().getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Resources.Status.ERROR)
+    }
+
+    @Test
+    // если поле price слишком велико
+    fun `insert shopping item with too long price return error`() {
+        // тестируем максимальную длину строки
+        val string = buildString {
+            for(i in 1..Constants.MAX_LENGTH_PRICE + 1) {
+                append(i)
+            }
+        }
+        viewModel.insertShoppingItems("name", "12", string)
+
+        val value = viewModel.insertShoppingStatus().getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Resources.Status.ERROR)
+    }
+
+    @Test
+    // если поле amount слишком велико
+    fun `insert shopping item with too long amount return error`() {
+        viewModel.insertShoppingItems("name", "1211111111111111111111111", "7.0")
+
+        val value = viewModel.insertShoppingStatus().getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Resources.Status.ERROR)
+    }
+
+    @Test
+    // если все поля введены коректно
+    fun `insert shopping item with valid input amount return SUCCESSES`() {
+        viewModel.insertShoppingItems("name", "12", "7.0")
+
+        val value = viewModel.insertShoppingStatus().getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Resources.Status.SUCCESSES)
+    }
+}
